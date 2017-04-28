199240a4cb21b68ec2748d4bb1e0f81e7dfe8a2e
thomasfanto <thomas.fanto@consid.se>
2017-04-28 08:39:38 +0200

    close but  . . .

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/mobileterminal/arquillian/TestTestRemoveMe.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/mobileterminal/arquillian/TestTestRemoveMe.java
@@ -1,7 +1,30 @@
 package eu.europa.ec.fisheries.uvms.mobileterminal.arquillian;
 
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 /**
  * Created by thofan on 2017-04-27.
  */
-public class TestTestRemoveMe {
+
+@RunWith(Arquillian.class)
+public class TestTestRemoveMe  extends BuildMovementTestDeployment{
+
+
+
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void test() {
+
+        double longitude = 9.140626D;
+        double latitude = 57.683805D;
+
+    }
+
+
+
 }
