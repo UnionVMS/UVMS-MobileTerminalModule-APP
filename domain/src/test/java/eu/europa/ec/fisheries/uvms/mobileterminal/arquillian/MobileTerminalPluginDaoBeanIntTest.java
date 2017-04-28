4776141a1be7afda8f7d0ebfae963ebc5fc3e962
Robert Larsson <robert.larsson@havochvatten.se>
2017-04-28 14:20:42 +0200

    Added test class.

---
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/mobileterminal/arquillian/MobileTerminalPluginDaoBeanIntTest.java
@@ -0,0 +1,32 @@
+package eu.europa.ec.fisheries.uvms.mobileterminal.arquillian;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by roblar on 2017-04-28.
+ */
+@RunWith(Arquillian.class)
+public class MobileTerminalPluginDaoBeanIntTest extends TransactionalTests {
+
+    @Test
+    public void testGetPluginList() {
+
+    }
+
+    @Test
+    public void testCreateMobileTerminalPlugin() {
+
+    }
+
+    @Test
+    public void testGetPluginByServiceName() {
+
+    }
+
+    @Test
+    public void testUpdatePlugin() {
+
+    }
+}
