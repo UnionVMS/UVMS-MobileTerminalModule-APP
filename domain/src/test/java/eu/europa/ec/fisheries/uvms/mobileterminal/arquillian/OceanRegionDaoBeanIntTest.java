d6b64f637fad8e745b61b9f30a7969008026e02f
Andreas Wallen <andreasw@Andreass-MacBook-Pro.local>
2017-05-02 15:20:37 +0200

    Changed name

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/mobileterminal/arquillian/OceanRegionDaoBeanIntTest.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/mobileterminal/arquillian/OceanRegionDaoBeanIntTest.java
@@ -23,9 +23,10 @@ public class OceanRegionDaoBeanIntTest extends TransactionalTests {
     @Test
     @OperateOnDeployment("normal")
     public void testGetOceanRegionList() throws ConfigDaoException {
+        // Since we have at least 4 regions inserted by LIQUIBASE this should always work
         List<OceanRegion> oceanRegions = oceanRegionDao.getOceanRegionList();
         Assert.assertNotNull(oceanRegions);
-        Assert.assertTrue(oceanRegions.size() > 0);
+        Assert.assertTrue(oceanRegions.size() > 3);
     }
 
 }
