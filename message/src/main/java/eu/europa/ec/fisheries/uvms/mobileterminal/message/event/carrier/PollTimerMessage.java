ea79f97910560a25092bfe5a8e79e0e08084b3ec
James Sörling <james.p.sorling@consid.se>
2017-09-22 13:04:22 +0200

    update model/events with toStringStyle Short.

--- message/src/main/java/eu/europa/ec/fisheries/uvms/mobileterminal/message/event/carrier/PollTimerMessage.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/mobileterminal/message/event/carrier/PollTimerMessage.java
@@ -11,6 +11,11 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
  */
 package eu.europa.ec.fisheries.uvms.mobileterminal.message.event.carrier;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class PollTimerMessage {
 
     private String message;
@@ -28,6 +33,21 @@ public class PollTimerMessage {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 
 }
