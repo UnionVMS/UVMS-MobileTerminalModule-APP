97b8e7b6ff007e7ae00a48a0f5ef17a73555d782
Kasim Gul <kasim.gul@havochvatten.se>
2018-04-24 16:29:46 +0200

    Refactoring and adding new It tests

--- service/src/test/java/eu/europa/fisheries/uvms/mobileterminal/service/arquillian/PollServiceBeanIntTest.java
+++ service/src/test/java/eu/europa/fisheries/uvms/mobileterminal/service/arquillian/PollServiceBeanIntTest.java
@@ -1,19 +1,32 @@
 package eu.europa.fisheries.uvms.mobileterminal.service.arquillian;
 
-import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.*;
+import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollAttribute;
+import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollAttributeType;
+import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollRequestType;
+import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollResponseType;
+import eu.europa.ec.fisheries.uvms.mobileterminal.dao.PollProgramDao;
 import eu.europa.ec.fisheries.uvms.mobileterminal.dao.exception.ConfigDaoException;
+import eu.europa.ec.fisheries.uvms.mobileterminal.dao.exception.PollDaoException;
 import eu.europa.ec.fisheries.uvms.mobileterminal.dao.exception.TerminalDaoException;
 import eu.europa.ec.fisheries.uvms.mobileterminal.dto.CreatePollResultDto;
+import eu.europa.ec.fisheries.uvms.mobileterminal.entity.poll.PollProgram;
 import eu.europa.ec.fisheries.uvms.mobileterminal.message.producer.bean.MessageProducerBean;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.PollService;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.exception.MobileTerminalServiceException;
 import eu.europa.fisheries.uvms.mobileterminal.service.arquillian.helper.TestPollHelper;
 import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
 public class PollServiceBeanIntTest extends TransactionalTests {
@@ -24,27 +37,127 @@ public class PollServiceBeanIntTest extends TransactionalTests {
     @EJB
     private TestPollHelper testPollHelper;
 
+    @EJB
+    private PollProgramDao pollProgramDao;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void createPoll() throws MobileTerminalServiceException, ConfigDaoException, TerminalDaoException {
         System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
         PollRequestType pollRequestType = testPollHelper.createPollRequestType();
         CreatePollResultDto createPollResultDto = pollService.createPoll(pollRequestType, "TEST");
-        Assert.assertNotNull(createPollResultDto);
+        assertNotNull(createPollResultDto);
     }
 
     @Test
-    public void createPollWithBrokenJMS() throws MobileTerminalServiceException, ConfigDaoException, TerminalDaoException {
+    public void createPollWithBrokenJMS_WillFail() throws ConfigDaoException, TerminalDaoException, MobileTerminalServiceException {
+
+        thrown.expect(MobileTerminalServiceException.class);
+        thrown.expectMessage("MESSAGE_PRODUCER_METHODS_FAIL == true");
+
         System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "true");
         PollRequestType pollRequestType = testPollHelper.createPollRequestType();
-        try {
-            pollService.createPoll(pollRequestType, "TEST");
-            Assert.fail();
-        } catch (MobileTerminalServiceException ignore) {
-        }
+        pollService.createPoll(pollRequestType, "TEST");
     }
 
+    @Test
+    public void getRunningProgramPolls() throws PollDaoException, MobileTerminalServiceException, ConfigDaoException, TerminalDaoException {
+        Date startDate = testPollHelper.getStartDate();
+        Date latestRun = testPollHelper.getLatestRunDate();
+        Date stopDate = testPollHelper.getStopDate();
 
+        String mobileTerminalSerialNumber = testPollHelper.createSerialNumber();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(mobileTerminalSerialNumber, startDate, stopDate, latestRun);
 
+        pollProgramDao.createPollProgram(pollProgram);
 
+        List<PollResponseType> runningProgramPolls = pollService.getRunningProgramPolls();
+        assertNotNull(runningProgramPolls);
+        assertEquals(1, runningProgramPolls.size());
+    }
 
+    @Test
+    public void startProgramPoll() throws ConfigDaoException, TerminalDaoException, PollDaoException, MobileTerminalServiceException {
+
+        System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
+
+        Date startDate = testPollHelper.getStartDate();
+        Date latestRun = testPollHelper.getLatestRunDate();
+        Date stopDate = testPollHelper.getStopDate();
+
+        String mobileTerminalSerialNumber = testPollHelper.createSerialNumber();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(mobileTerminalSerialNumber, startDate, stopDate, latestRun);
+
+        pollProgramDao.createPollProgram(pollProgram);
+
+        PollResponseType responseType = pollService.startProgramPoll(pollProgram.getGuid(), pollProgram.getUpdatedBy());
+        List<PollAttribute> attributes = responseType.getAttributes();
+
+        boolean ok = false;
+        for(PollAttribute attribute : attributes) {
+            if(attribute.getKey().toString().equalsIgnoreCase(PollAttributeType.PROGRAM_RUNNING.toString())) {
+                assertEquals(attribute.getValue(), "TRUE");
+                ok = true;
+            }
+        }
+        assertTrue(ok);
+        assertNotNull(responseType);
+    }
+
+    @Test
+    public void stopProgramPoll() throws ConfigDaoException, TerminalDaoException, PollDaoException, MobileTerminalServiceException {
+
+        System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
+
+        Date startDate = testPollHelper.getStartDate();
+        Date latestRun = testPollHelper.getLatestRunDate();
+        Date stopDate = testPollHelper.getStopDate();
+
+        String mobileTerminalSerialNumber = testPollHelper.createSerialNumber();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(mobileTerminalSerialNumber, startDate, stopDate, latestRun);
+
+        pollProgramDao.createPollProgram(pollProgram);
+
+        PollResponseType responseType = pollService.stopProgramPoll(pollProgram.getGuid(), pollProgram.getUpdatedBy());
+        List<PollAttribute> attributes = responseType.getAttributes();
+
+        boolean ok = false;
+        for(PollAttribute attribute : attributes) {
+            if(attribute.getKey().toString().equalsIgnoreCase(PollAttributeType.PROGRAM_RUNNING.toString())) {
+                assertEquals(attribute.getValue(), "FALSE");
+                ok = true;
+            }
+        }
+        assertTrue(ok);
+        assertNotNull(responseType);
+    }
+
+    @Test
+    public void inactivateProgramPoll() throws PollDaoException, ConfigDaoException, TerminalDaoException, MobileTerminalServiceException {
+        System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
+
+        Date startDate = testPollHelper.getStartDate();
+        Date latestRun = testPollHelper.getLatestRunDate();
+        Date stopDate = testPollHelper.getStopDate();
+
+        String mobileTerminalSerialNumber = testPollHelper.createSerialNumber();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(mobileTerminalSerialNumber, startDate, stopDate, latestRun);
+
+        pollProgramDao.createPollProgram(pollProgram);
+
+        PollResponseType responseType = pollService.inactivateProgramPoll(pollProgram.getGuid(), pollProgram.getUpdatedBy());
+        List<PollAttribute> attributes = responseType.getAttributes();
+
+        boolean ok = false;
+        for(PollAttribute attribute : attributes) {
+            if(attribute.getKey().toString().equalsIgnoreCase(PollAttributeType.PROGRAM_RUNNING.toString())) {
+                assertEquals(attribute.getValue(), "FALSE");
+                ok = true;
+            }
+        }
+        assertTrue(ok);
+        assertNotNull(responseType);
+    }
 }
